b0144fe4de14d72e5965bf8b5e47baa4e6d8aec6
Christine Susic <csusic022@insite.4cd.edu>
2026-03-01 16:43:16 -0800

    Commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // COMSC-210 | Lab 14 | Christine Susic
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 const int W1 = 10;
@@ -10,11 +11,11 @@ private:
     int red, green, blue;
 public:
     int getRed() const      {return red;}      
-    void setRed()           {red = r;}
+    void setRed();          
     int getGreen() const    {return green;}      
-    void setGreen()         {green = g;}
+    void setGreen();        
     int getBlue() const     {return blue;}  
-    void setBlue()          {blue = b;}
+    void setBlue();          
     //print
     void print() {
         cout << setw(W1) << "Red:" << red << endl;
@@ -24,11 +25,24 @@ public:
 };
 
 int main() {
-    //Red
-    
-    //Green
-    
-    //Blue
+    //Color1
+    Color color1;
+    color1.setRed(100);
+    color1.setGreen(200);
+    color1.setBlue(135);
+    color1.print();
+    //Color2
+    Color color2;
+    color2.setRed(215);
+    color2.setGreen(123);
+    color2.setBlue(15);
+    color2.print();
+    //Color3
+    Color color3;
+    color3.setRed(22);
+    color3.setGreen(123);
+    color3.setBlue(56);
+    color3.print();
     
     return 0;
 }
