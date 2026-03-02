e59d5bd287788cf7caa5540789614fc57d25f7eb
Christine Susic <csusic022@insite.4cd.edu>
2026-03-01 16:33:27 -0800

    Commit

--- main.cpp
+++ main.cpp
@@ -3,22 +3,32 @@
 #include <iostream>
 using namespace std;
 
+const int W1 = 10;
+
 class Color {
 private: 
     int red, green, blue;
 public:
-    int getRed() const;
-    void setRed();
-    int getGreen() const;
-    void setGreen();
-    int getBlue() const;
-    void setBlue();
-    void print();
-    
+    int getRed() const      {return red;}      
+    void setRed()           {red = r;}
+    int getGreen() const    {return green;}      
+    void setGreen()         {green = g;}
+    int getBlue() const     {return blue;}  
+    void setBlue()          {blue = b;}
+    //print
+    void print() {
+        cout << setw(W1) << "Red:" << red << endl;
+        cout << setw(W1) << "Green:" << green << endl;
+        cout << setw(W1) << "Blue:" << blue << endl;
+    }
 };
 
 int main() {
+    //Red
     
+    //Green
+    
+    //Blue
     
     return 0;
 }
