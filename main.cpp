5c93d2b405f5b3f676fe84e981fe0b92d108fe8b
Christine Susic <csusic022@insite.4cd.edu>
2026-03-01 16:23:17 -0800

    Commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// COMSC-210 | Lab 14 | Christine Susic
+
+#include <iostream>
+using namespace std;
+
+class Color {
+private: 
+    int red, green, blue;
+public:
+    int getRed() const;
+    void setRed();
+    int getGreen() const;
+    void setGreen();
+    int getBlue() const;
+    void setBlue();
+    void print();
+    
+};
+
+int main() {
+    
+    
+    return 0;
+}
